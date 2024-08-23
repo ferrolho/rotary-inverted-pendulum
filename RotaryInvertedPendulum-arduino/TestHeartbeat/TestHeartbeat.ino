9b95f53a7abfa44a8172d3e3391be2b7c2cc64f9
Henrique Ferrolho <henrique.ferrolho@gmail.com>
2024-08-23 09:21:14 +0100

    Add Arduino sketch to test hearbeat

---
+++ RotaryInvertedPendulum-arduino/TestHeartbeat/TestHeartbeat.ino
@@ -0,0 +1,16 @@
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+}
